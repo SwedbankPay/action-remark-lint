e8911bf60d7c1a25cc0543e2fae280110cd93391
Daniel Granerud <daniel.granerud@payex.com>
2020-05-26 13:58:30 +0200

    Try reviewdog

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,16 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
   # erroformat: https://git.io/JeGMU
-  echo "ERROR: github-pr-review unsupported (for now)"
-  exit 1
+  remark --quiet . 2>&1 |
+  reviewdog \
+    -efm="%f\n%l:%c: %m" \
+    -name="remark-lint" \
+    -reporter="github-pr-review" \
+    -level="${INPUT_LEVEL}" \
+    -filter-mode="${INPUT_FILTER_MODE}" \
+    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+    -level="${INPUT_LEVEL}" \
+    ${INPUT_REVIEWDOG_FLAGS}
 else
   # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
   remark --quiet --use=remark-preset-lint-recommended . 2>&1 |
