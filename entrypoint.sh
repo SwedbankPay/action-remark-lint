44968fb9548975f2487299c5fbe6fa96c78a66ae
Daniel Granerud <daniel.granerud@payex.com>
2020-05-27 10:37:24 +0200

    Simplify script and run npm install

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,13 @@ cd "${GITHUB_WORKSPACE}" || exit
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
-  # erroformat: https://git.io/JeGMU
-  remark --quiet . 2>&1 |
-  reviewdog \
-    -efm="%-P%f\ %#%l:%c %# %trror  %m\ %s %# %trror  %m\ %#%l:%c %# %tarning  %m\ %s %# %tarning  %m\ %-Q\ %-G%.%#" \
-    -name="remark-lint" \
-    -diff="git diff master" \
-    -reporter="github-pr-review" \
-    -level="${INPUT_LEVEL:-info}"
-    ${INPUT_REVIEWDOG_FLAGS}
-else
-  # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
-  remark --quiet --use=remark-preset-lint-recommended . 2>&1 |
-    reviewdog -efm="%f\n%l:%c: %m" -name="remark-lint" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}" -tee
-fi
+npm install
+
+remark --quiet . 2>&1 | reviewdog \
+  -efm="%-P%f\ %#%l:%c %# %trror  %m\ %s %# %trror  %m\ %#%l:%c %# %tarning  %m\ %s %# %tarning  %m\ %-Q\ %-G%.%#" \
+  -name="remark-lint" \
+  -diff="git diff master" \
+  -reporter="github-pr-review" \
+  -level="${INPUT_LEVEL:-info}" \
+  ${INPUT_REVIEWDOG_FLAGS} \
+  -tee
